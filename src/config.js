98bc87cab4daa337a1e131a2287ced109db8f4ee
sidhujag <sidhujag@gmail.com>
2019-05-14 14:22:31 -0700

    update contracts and status check

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
       this.agentURL = "localhost";
       this.agentPort = 8000;
       this.infuraURL = "https://rinkeby.infura.io/v3/d178aecf49154b12be98e68e998cfb8d";
-      this.superblockContract = "0x90c6441a2231cbc57f4aa5e4e72f2c85b7170234";
+      this.superblockContract = "0x7859a0a3f54dcbe3dbd47176452d021265f9b23d";
     }
   }
   export default new CONFIGURATION();
