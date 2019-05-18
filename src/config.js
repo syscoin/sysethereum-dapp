5128a763b23986b238410444f165ec2484da5e44
sidhujag <sidhujag@gmail.com>
2019-05-17 20:38:29 -0700

    updste some params

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
   class CONFIGURATION {
     constructor() {
-      this.testnet = false;
+      this.testnet = true;
       this.agentURL = "localhost";
       this.agentPort = 8000;
       this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
