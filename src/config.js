b453e59fceacbdf0f003fb655d65ea045a64041e
sidhujag <sidhujag@gmail.com>
2019-05-28 12:49:44 -0700

    updated dapp contracts

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
       this.agentURL = "localhost";
       this.agentPort = 8000;
       this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
-      this.superblockContract = "0xecee3b8cbd3ce1176f12069b394d4da0ef3fa7ef";
+      this.superblockContract = "0x54d6f61da113e125aa95f24f3a7ee671fdce8f9d";
     }
   }
   export default new CONFIGURATION();
