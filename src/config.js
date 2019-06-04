ac007557cfbec91030a1db5122167ce256c0b779
sidhujag <sidhujag@gmail.com>
2019-06-04 13:21:10 -0700

    update dapp to new superblock

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@
       this.testnet = true;
       this.agentURL = "localhost";
       this.agentPort = 8000;
-      this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
-      this.superblockContract = "0x680db3caa35d2ee1237cae45aa1aa6b550b73848";
+      this.infuraURL = "https://mainnet.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
+      this.superblockContract = "0x849c4b350a77269f65e2386cc6eed9b2f89281cc";
     }
   }
   export default new CONFIGURATION();
