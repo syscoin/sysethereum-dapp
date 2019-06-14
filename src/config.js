616cb3939e3099a9420856a2a2872945c3017715
sidhujag <sidhujag@gmail.com>
2019-06-13 17:11:02 -0700

    update sb

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
   class CONFIGURATION {
     constructor() {
-      this.testnet = false;
+      this.testnet = true;
       this.agentURL = "localhost";
       this.agentPort = 8000;
-      this.infuraURL = "https://mainnet.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
-      this.superblockContract = "0xced7ce5c336bb31f35bc5bdc86c651c7136b3825";
+      this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
+      this.superblockContract = "0x753f0a04e660fc35a5620611e1c42fe4cf18f35d";
     }
   }
   export default new CONFIGURATION();
