664175dc75217104bf56e572111060760b88321e
sidhujag <sidhujag@gmail.com>
2019-07-02 16:45:23 -0700

    update contract

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
       this.agentURL = "localhost";
       this.agentPort = 8000;
       this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
-      this.superblockContract = "0xa7ce0410Be6451D5A1a6b25A0998E4671da5F96d";
+      this.superblockContract = "0x421E73053031a1aaa2Aee53bBC23Bf5E574E3071";
     }
   }
   export default new CONFIGURATION();
--- src/wizard/i18n.js
+++ src/wizard/i18n.js
@@ -41,7 +41,7 @@ i18n.use(LanguageDetector).init({
         'step1SuperblockParentId': 'Previous Superblock ID',
         'step1SearchBox': 'Enter a hash or height to locate a Superblock...',
         'step1ESHead': 'Welcome to the official Ethereum to Syscoin Bridge wizard!',
-        'step1ESDescription': 'This is a wizard for a bridge for users of Ethereum who has previously walked over from Syscoin to move back trustlessly. An SPV proof of the Ethereum burn transaction is required to move back to Syscoin. Please enter details about which asset to move back to Syscoin. Enter "0" for Asset GUID and "0x969ac050573eD220d9F70F7e235A02BB3dD1A222" as the Ethereum SYSX Contract if moving Syscoin over otherwise enter the GUID and contract of the Asset token you are moving back to Syscoin.',
+        'step1ESDescription': 'This is a wizard for a bridge for users of Ethereum who has previously walked over from Syscoin to move back trustlessly. An SPV proof of the Ethereum burn transaction is required to move back to Syscoin. Please enter details about which asset to move back to Syscoin. Enter "0" for Asset GUID and "0x75963d8B2561Ba60B24c10425b13017ad8170522" as the Ethereum SYSX Contract if moving Syscoin over otherwise enter the GUID and contract of the Asset token you are moving back to Syscoin.',
         'step1ESContractLabel': 'Ethereum SYSX Contract',
         'step1SEContract': 'A valid Ethereum contract representing the SYSX token is required',
         'step1ESEnterContract': 'Enter contract address...',
