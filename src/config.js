ff60539fc4cab2d14d512c4891d1586ec3d2ffd4
sidhujag <sidhujag@gmail.com>
2019-08-19 11:41:49 -0700

    remove api key from infura

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
       this.testnet = true;
       this.agentURL = "localhost";
       this.agentPort = 8000;
-      this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
+      this.infuraURL = "https://rinkeby.infura.io/v3/xxx";
       this.superblockContract = "0x12974fFc30231FBF254588f6aa647A26EdA547af";
       this.ERC20Manager = "0x890345340A36C7C4e24a5a6ea0a7AE87E223DD38";
     }
