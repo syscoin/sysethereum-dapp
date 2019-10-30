6e327f2d4abc2cf672f0a12c4020cb148ba6df94
anzem <yocker@gmail.com>
2019-10-30 10:28:28 +0100

    updated faqs, faucet links

--- src/SysethereumDApp.js
+++ src/SysethereumDApp.js
@@ -67,7 +67,7 @@ class SysethereumDApp extends Component {
         <div className="intro">
 
             <div className="left">
-              <a href="https://syscoin.org/" target="_blank" className="logo"></a>
+              <a href="https://syscoin.org/" target="_blank" rel="noopener noreferrer" className="logo"></a>
               <h1>Syscoin - Ethereum Bridge</h1>
 
               <div className="bottom">
@@ -140,22 +140,22 @@ class SysethereumDApp extends Component {
               </div>
 
               <div className="links">
-                <a href="https://twitter.com/syscoin" target="_blank" title="Twitter">
+                <a href="https://twitter.com/syscoin" target="_blank" rel="noopener noreferrer" title="Twitter">
                   <i className="demo-icon icon-twitter"></i>
                 </a>
-                <a href="https://t.me/Syscoin_Official" target="_blank" title="Telegram">
+                <a href="https://t.me/Syscoin_Official" target="_blank" rel="noopener noreferrer" title="Telegram">
                   <i className="demo-icon icon-icon-telegram"></i>
                 </a>
-                <a href="https://discord.gg/RkK2AXD" target="_blank" title="Discord">
+                <a href="https://discord.gg/RkK2AXD" target="_blank" rel="noopener noreferrer" title="Discord">
                   <i className="demo-icon icon-discord-logo-white"></i>
                 </a>
-                <a href="https://www.reddit.com/r/SysCoin/" target="_blank" title="Reddit">
+                <a href="https://www.reddit.com/r/SysCoin/" target="_blank" rel="noopener noreferrer" title="Reddit">
                   <i className="demo-icon icon-reddit-alien"></i>
                 </a>
-                <a href="https://github.com/syscoin" target="_blank" title="Github">
+                <a href="https://github.com/syscoin" target="_blank" rel="noopener noreferrer" title="Github">
                   <i className="demo-icon icon-github-circled"></i>
                 </a>
-                <a href="https://syscoin.org" target="_blank" title="Syscoin.org">
+                <a href="https://syscoin.org" target="_blank" rel="noopener noreferrer" title="Syscoin.org">
                   <i className="demo-icon icon-sys"></i>
                 </a>
               </div>
@@ -246,7 +246,7 @@ class SysethereumDApp extends Component {
             </p>
 
             <p>
-              <a href="https://rinkeby.etherscan.io/address/0x7d5602305f4d2c7dc9c85b40a47d88b63086eb2b#code" target="_blank">Check out the Superblock contract on Rinkeby testnet</a>
+              <a href="https://rinkeby.etherscan.io/address/0x7d5602305f4d2c7dc9c85b40a47d88b63086eb2b#code" target="_blank" rel="noopener noreferrer">Check out the Superblock contract on Rinkeby testnet</a>
             </p>
 
             <div className="ornament intext"></div>
@@ -278,9 +278,9 @@ class SysethereumDApp extends Component {
             </p>
 
             <p>
-              <a href="https://github.com/syscoin/sysethereum-contracts" target="_blank">View Contract code</a><br />
-              <a href="https://github.com/syscoin/sysethereum-dapp" target="_blank">View DApp code</a><br />
-              <a href="https://github.com/syscoin/sysethereum-agents" target="_blank">View Agent code</a>
+              <a href="https://github.com/syscoin/sysethereum-contracts" target="_blank" rel="noopener noreferrer">View Contract code</a><br />
+              <a href="https://github.com/syscoin/sysethereum-dapp" target="_blank" rel="noopener noreferrer">View DApp code</a><br />
+              <a href="https://github.com/syscoin/sysethereum-agents" target="_blank" rel="noopener noreferrer">View Agent code</a>
             </p>
 
           </div>
@@ -293,39 +293,122 @@ class SysethereumDApp extends Component {
             <h1>FAQ</h1>
 
             <p className="question">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            What is an SPT?
             </p>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non tellus sed quam consectetur ultrices. Sed iaculis eget elit id imperdiet. Vivamus ex ligula, commodo sit amet ullamcorper et, facilisis non nisi. Etiam ornare aliquam vestibulum. Fusce quis lacus vitae enim egestas egestas luctus sit amet odio. Suspendisse fringilla commodo tellus, id scelerisque enim porta vel. Phasellus tempus, quam et maximus luctus, mi ante sodales sapien, eget rutrum nibh urna sit amet augue.
+            Syscoin Platform Tokens (SPT) allow anyone to quickly create their own cryptocurrency token on Syscoin Platform.
             </p>
             
             <p className="question">
-            Praesent imperdiet, eros elementum semper pulvinar, quam nisl luctus ipsum, ac mattis mi quam id ipsum. Maecenas nec augue quis tellus aliquam commodo.
+            What is SYSX?
             </p>
             <p>
-          Aliquam erat volutpat. Proin et ex et felis auctor consectetur. Praesent imperdiet, eros elementum semper pulvinar, quam nisl luctus ipsum, ac mattis mi quam id ipsum. Maecenas nec augue quis tellus aliquam commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum facilisis felis vitae orci condimentum congue. Nulla facilisi.
+            SYSX is an SPT and ETH ERC20 token, backed by Syscoin 1:1. You will be able to burn your Syscoin and mint SYSX on the Syscoin chain as a token, allowing you to utilize Z-DAG. You can then burn your SYSX and mint a SYSX (ERC20) token, allowing you to utilize all the functionalities of the Ethereum Chain such as Smart Contracts.
           </p>
 
           <p className="question">
-          Quisque erat mi, condimentum sed magna vel, ultricies mollis est
+          How does the SYSX bridge work?
             </p>
           <p>
-          Suspendisse erat dolor, ultricies vel aliquam molestie, fermentum non arcu. Mauris erat metus, cursus non posuere at, commodo ac orci. Duis luctus massa dolor. Duis luctus dolor ultrices, feugiat turpis nec, venenatis ex. Suspendisse dictum ipsum nec libero volutpat, ut posuere tortor ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus mollis non felis vitae consequat. Donec faucibus, nisl at vulputate congue, neque justo pretium velit, non porttitor massa justo eu sapien. Ut dapibus tincidunt malesuada. Aenean at vestibulum velit, tristique tempor ex. Aliquam quis turpis interdum, consectetur felis ut, molestie est. Praesent lobortis, ligula quis pharetra laoreet, nibh orci suscipit neque, finibus viverra mauris felis in augue. Aliquam tortor nulla, porta facilisis tempus sed, dapibus non libero.
+          The basic structure of how SYS bridge works is SYS &lt;&gt; SYSX &lt;&gt; SYSETH (ERC20).
+          </p>
+          <p>
+          You will need to burn your Syscoin for the SYSX SPT. You can then move your SYSX SPT across the bridge, which will then be minted as a SYSX (ERC20). 
+          </p>
+          <p>
+          The total supply of SYS + SYSX + SYSX (ERC20) = Total Circulating Supply.
           </p>
 
           <p className="question">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          Does the EVM run on Ethereum?
             </p>
           <p>
-          Morbi placerat massa ac iaculis dapibus. Maecenas id nisi sit amet lorem vestibulum sagittis. Nam sit amet vestibulum ipsum. Nunc tincidunt purus eget ex scelerisque vestibulum. Donec lobortis, velit a porttitor pellentesque, diam dolor feugiat velit, quis rutrum nibh magna ut velit. Ut varius arcu consectetur ligula aliquet fermentum id ut lorem. Nunc sed leo et arcu molestie tincidunt.
+          Yes the EVM will run on Ethereum as the name says, EVM stands for Ethereum Virtual Machine.
           </p>
 
           <p className="question">
-          Morbi placerat massa ac iaculis dapibus. Maecenas id nisi sit amet lorem vestibulum sagittis.
+          Will SPT’s be transferable to ETH?
             </p>
           <p>
-          Nunc pulvinar sed lacus ut malesuada. Duis volutpat ante pretium ipsum iaculis porttitor. Proin venenatis sed diam eget porta. Pellentesque ultrices, libero vitae dictum ultricies, ante sapien scelerisque metus, et egestas sapien ipsum in quam. Suspendisse et metus eget turpis suscipit pharetra ac et tortor. Quisque erat mi, condimentum sed magna vel, ultricies mollis est. Maecenas venenatis dolor quis augue faucibus vestibulum. Proin sodales sed metus aliquet dictum. Pellentesque mi neque, fringilla et metus sed, dictum euismod orci. In molestie eu nunc ut aliquam. Etiam tempus purus ac eros semper placerat. In maximus fermentum augue, ut interdum tortor consequat ac. Suspendisse suscipit facilisis consectetur. Praesent id eleifend dui. Suspendisse imperdiet ex non consectetur volutpat. Praesent hendrerit fermentum mi, sed laoreet turpis elementum vel.
+          SPT’s will be able to be transferred to Ethereum as ERC20 tokens and back to Syscoin SPT’s if needed.
           </p>
+
+          <p className="question">
+          Will this mean I can use Ledger, Myetherwallet, Metamask etc?
+          </p>
+          <p>
+          Yes this will mean that your SPT, will be compatible with all the major service providers of Ethereum if you have moved them across the bridge to an ERC20 token.
+          </p>
+
+          <p className="question">
+          Syscoin supply will remain the same?
+          </p>
+          <p>
+          Yes. The total supply of SYS + SYSX + SYSX (ERC20) = Total Circulating Supply.
+          </p>
+
+          <p className="question">
+          Can I have SYSX in my QT/Spark wallet?
+          </p>
+          <p>
+          SYSX is an SPT and an ERC20 token. It will depend on what chain you are on. If you are on the ETH chain, you will need to use the Ethereum supported wallets. If you are on the Syscoin Chain you will need to use Spark. QT can also be used, although you will need to have sound knowledge on cli commands.
+
+          </p>
+
+          <p className="question">
+          Do I need Ether to execute the smart contract, and if so how much?
+          </p>
+          <p>
+          Yes you will need Ether to execute a smart contract to cover the costs of executing a smart contract command. Cost will depend on current network.
+          </p>
+
+          <p className="question">
+          Do I need Sys to execute ZDAG transactions, and if so how much?
+          </p>
+          <p>
+          Yes you will need Syscoin to execute any transaction on the Syscoin network including ZDAG asset transfers. Cost will depend on current network situation however it is relatively cheap, thousands of transactions per SYS currently and cheaper with ZDAG because it offers a unique fee market since users do not usually require having confirmations within an immediate block.
+          </p>
+
+          <p className="question">
+          Any specific uses of smart contracts for Syscoin specifically?
+          </p>
+          <p>
+          As discussed in the blogpost on the evolution on Syscoin there are multiple ways Syscoin can leverage smart contracts. Since it’s a two way bridge, smart contracts on Ethereum will also be able to leverage advantages specific for Syscoin such as Z-DAG speed and throughput. Users on Syscoin also benefit from the security of merged-mining with the Bitcoin network. A network resilient against 51% attacks is of utmost concern when dealing with a base layer blockchain and deciding where to store your assets.
+          </p>
+
+          <p className="question">
+          Will Z-DAG be as fast on SYSETH, or will it be bounded to ETH speed?
+          </p>
+          <p>
+          Since SYSX ERC20 lives inside the EVM, Z-DAG won’t affect internal transfers of SYSX. SYSX on the Syscoin chain will be able to utilize Z-Dag. 
+          </p>
+
+          <p className="question">
+          Why have an SPT on Syscoin if I can have an ERC20 token?
+          </p>
+          <p>
+          SPT’s are driven by Z-DAG which will allow for high speed and high throughput token transfers, especially attractive in point of sale applications.
+          </p>
+
+          <p className="question">
+          Can other ERC20 tokens be migrated to Syscoin chain for fast transferring and Point of Sale applications?
+          </p>
+          <p>
+          Yes ERC20 will be able to leverage Z-DAG speed and throughput by using the bridge and transform themselves into an SPT.
+          </p>
+
+          <p className="question">
+          How is this initiative different than others?
+          </p>
+          <p>
+          Sysethereum is the first two-way interoperability solution without counterparties, as a permissionless and trustless solution that leverages the security of each respective blockchain. This allows us to consider the SPT supply mechanism as a fractional supply across multiple blockchains. Users on Syscoin by extension will be able to leverage the vast toolset and network effect of Ethereum whilst Ethereum users can leverage Syscoin’s cost effective and efficient asset specific transactionality. You can read more about the technicals here: <a href="https://github.com/syscoin/sysethereum-docs" target="_blank" rel="noopener noreferrer">https://github.com/syscoin/sysethereum-docs</a>
+          </p>
+
+
+
+
+
+
           </div>
         </div>
 
@@ -370,31 +453,32 @@ class SysethereumDApp extends Component {
             <h1>More information</h1>
 
             <div className="links">
-              <a href="https://syscoin.org" target="_blank">Syscoin.org</a>
-              <a href="https://syscoin.network/syslinks/" target="_blank">Syscoin links</a>
-              <a href="https://syscoin.readme.io/" target="_blank">Developer Portal</a>
-              <a href="http://explorer.blockchainfoundry.co/" target="_blank">Syscoin explorer</a>
-              <a href="https://syscoin.org/whitepaper" target="_blank">Syscoin Whitepapers</a>
-              <a href="https://github.com/syscoin" target="_blank">Syscoin Github</a>
+              <a href="https://syscoin.org" target="_blank" rel="noopener noreferrer">Syscoin.org</a>
+              <a href="https://faucet.syscoin.org" target="_blank" rel="noopener noreferrer">Syscoin faucet</a>
+              <a href="https://syscoin.network/syslinks/" target="_blank" rel="noopener noreferrer">Syscoin links</a>
+              <a href="https://syscoin.readme.io/" target="_blank" rel="noopener noreferrer">Developer Portal</a>
+              <a href="http://explorer.blockchainfoundry.co/" target="_blank" rel="noopener noreferrer">Syscoin explorer</a>
+              <a href="https://syscoin.org/whitepaper" target="_blank" rel="noopener noreferrer">Syscoin Whitepapers</a>
+              <a href="https://github.com/syscoin" target="_blank" rel="noopener noreferrer">Syscoin Github</a>
             </div>
 
             <div className="linksSocial">
-                <a href="https://twitter.com/syscoin" target="_blank" title="Twitter">
+                <a href="https://twitter.com/syscoin" target="_blank" rel="noopener noreferrer" title="Twitter">
                   <i className="demo-icon icon-twitter"></i>
                 </a>
-                <a href="https://t.me/Syscoin_Official" target="_blank" title="Telegram">
+                <a href="https://t.me/Syscoin_Official" target="_blank" rel="noopener noreferrer" title="Telegram">
                   <i className="demo-icon icon-icon-telegram"></i>
                 </a>
-                <a href="https://discord.gg/RkK2AXD" target="_blank" title="Discord">
+                <a href="https://discord.gg/RkK2AXD" target="_blank" rel="noopener noreferrer" title="Discord">
                   <i className="demo-icon icon-discord-logo-white"></i>
                 </a>
-                <a href="https://www.reddit.com/r/SysCoin/" target="_blank" title="Reddit">
+                <a href="https://www.reddit.com/r/SysCoin/" target="_blank" rel="noopener noreferrer" title="Reddit">
                   <i className="demo-icon icon-reddit-alien"></i>
                 </a>
-                <a href="https://github.com/syscoin" target="_blank" title="Github">
+                <a href="https://github.com/syscoin" target="_blank" rel="noopener noreferrer" title="Github">
                   <i className="demo-icon icon-github-circled"></i>
                 </a>
-                <a href="https://syscoin.org" target="_blank" title="Syscoin.org">
+                <a href="https://syscoin.org" target="_blank" rel="noopener noreferrer" title="Syscoin.org">
                   <i className="demo-icon icon-sys"></i>
                 </a>
             </div>
