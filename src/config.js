412f8fc5d0e27ea63b4b40638f96866ae035d8cc
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-01 13:49:54 -0700

    use 8443

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
     constructor() {
       this.testnet = true;
       this.agentURL = "localhost";
-      this.agentPort = 8000;
+      this.agentPort = 8443;
       this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
       this.superblockContract = "0xF0E3dc88AeeB48d000a409Ac3F9163200439897A";
       this.ERC20Manager = "0x460e8C8923881Cf37d7302dBEE3350DbFC19B0C4";
