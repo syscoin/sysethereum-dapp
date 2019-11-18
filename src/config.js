30871e8d1f3292673287b2b3d38e73be9ea8af63
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-18 14:15:41 -0800

    update for testnet

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
       this.agentPort = 8443;
       this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
       this.superblockContract = "0xF0E3dc88AeeB48d000a409Ac3F9163200439897A";
-      this.ERC20Manager = "0x460e8C8923881Cf37d7302dBEE3350DbFC19B0C4";
+      this.ERC20Manager = "0x25654E66a7c18F4D9D5391D9f8a58820fF034c1C";
     }
   }
   export default new CONFIGURATION();
