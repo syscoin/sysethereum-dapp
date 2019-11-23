31f36219bfbf6ca27c2026159a2a804b76c2ce06
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-22 20:34:31 -0800

    remove https

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
   class CONFIGURATION {
     constructor() {
       this.testnet = false;
-      this.agentURL = "https://bridge.syscoin.org";
+      this.agentURL = "bridge.syscoin.org";
       this.agentPort = 8443;
       this.infuraURL = "https://mainnet.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
       this.superblockContract = "0x7a73f70ca54Ac0616bcB4c5Cca9787eDbAd950cE";
