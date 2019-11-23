87131d83635defc5dfa6bcc40b6a1052bcfc3c54
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-23 10:02:59 -0800

    update for testnet

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
   class CONFIGURATION {
     constructor() {
-      this.testnet = false;
-      this.agentURL = "bridge.syscoin.org";
+      this.testnet = true;
+      this.agentURL = "bridge-testnet.syscoin.org";
       this.agentPort = 8443;
-      this.infuraURL = "https://mainnet.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
-      this.superblockContract = "0x7a73f70ca54Ac0616bcB4c5Cca9787eDbAd950cE";
-      this.ERC20Manager = "0x30929228931464ee18AF52b9f655F8176555b6e7";
+      this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
+      this.superblockContract = "0xd745BC855e72e8535a929B13Aaa630894d43A2bd";
+      this.ERC20Manager = "0x38945d8004cf4671c45686853452A6510812117c";
     }
   }
   export default new CONFIGURATION();
