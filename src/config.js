09552ca512d81f5350ff14896cbe86a9e3134901
sidhujag <jsidhu@blockchainfoundry.co>
2019-12-20 12:29:52 -0800

    update mainnet params

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@
       this.testnet = false;
       this.agentURL = "bridge.syscoin.org";
       this.agentPort = 8443;
-      this.infuraURL = "https://rinkeby.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
-      this.superblockContract = "0x0c206e62dB7478C7D55B5a8C39cACe72FCa29ef7";
-      this.ERC20Manager = "0x443d9a14fb6ba2A45465bEC3767186f404Ccea25";
+      this.infuraURL = "https://mainnet.infura.io/v3/b168da11b4da49b0aee04717e7904a06";
+      this.superblockContract = "0xa0Fe740Ed57C9FCB0A04E330824aD8aD574bc54c";
+      this.ERC20Manager = "0xFF957eA28b537b34E0c6E6B50c6c938668DD28a0";
       this.tokenFreezeFunction = "0xaabab1db49e504b5156edf3f99042aeecb9607a08f392589571cd49743aaba8d";
     }
   }
