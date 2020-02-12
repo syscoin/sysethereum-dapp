e31666ab99d95519ce0457cd27ae432527d693e0
unknown <yocker@gmail.com>
2020-02-12 15:06:55 +0100

    added google tag manager

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import SysethereumDApp from './SysethereumDApp';
 import * as serviceWorker from './serviceWorker';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-KC8L4Z2'
+}
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(<SysethereumDApp />, document.getElementById('root'));
 
@@ -10,3 +17,4 @@ ReactDOM.render(<SysethereumDApp />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
