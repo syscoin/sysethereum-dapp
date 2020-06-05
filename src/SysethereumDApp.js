dc970cd199b305313e8fe76f39a8acc69e96d72d
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-05 14:36:57 -0700

    fix link

--- src/SysethereumDApp.js
+++ src/SysethereumDApp.js
@@ -492,7 +492,7 @@ class SysethereumDApp extends Component {
               <a href="https://faucet.syscoin.org" target="_blank" rel="noopener noreferrer">Syscoin faucet</a>
               <a href="https://syscoin.network/syslinks/" target="_blank" rel="noopener noreferrer">Syscoin links</a>
               <a href="https://syscoin.readme.io/" target="_blank" rel="noopener noreferrer">Developer Portal</a>
-              <a href="https://sys1.bcfn.org/" target="_blank" rel="noopener noreferrer">Syscoin explorer</a>
+              <a href="https://sys1.bcfn.ca/" target="_blank" rel="noopener noreferrer">Syscoin explorer</a>
               <a href="https://syscoin.org/whitepaper" target="_blank" rel="noopener noreferrer">Syscoin Whitepapers</a>
               <a href="https://github.com/syscoin" target="_blank" rel="noopener noreferrer">Syscoin Github</a>
             </div>
