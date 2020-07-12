f5b9127f620e6b15923b1a6e84cc9a92767431cd
sidhujag <jsidhu@blockchainfoundry.co>
2020-07-12 10:22:17 -0700

    remove need to push tx and recp roots

--- src/wizard/Step2EthToSys.js
+++ src/wizard/Step2EthToSys.js
@@ -127,27 +127,23 @@ class Step2ES extends Component {
         }
         let result = await buildEthProof.getTransactionProof(ethTXID);
         let tx_hex = rlp.encode(result.value).toString('hex');
-        let tx_root_hex = rlp.encode(result.header[4]).toString('hex') ;
         let txmerkleproof_hex =  rlp.encode(result.parentNodes).toString('hex');
         let txmerkleproofpath_hex = result.path.toString('hex');
         let blockNumber = result.blockNumber || this.props.getStore().receiptBlocknumber;
 
         result = await buildEthProof.getReceiptProof(ethTXID);
         let receipt_hex = rlp.encode(result.value).toString('hex');
-        let receipt_root_hex = rlp.encode(result.header[5]).toString('hex') ;
         let receiptmerkleproof_hex =  rlp.encode(result.parentNodes).toString('hex');
 
-        console.log("tx_root_hex " + tx_root_hex);
         console.log("tx_hex: " + tx_hex);
         console.log("txmerkleproof_hex: " + txmerkleproof_hex);
         console.log("txmerkleproofpath_hex: " + txmerkleproofpath_hex);
         
-        console.log("receipt_root_hex " + receipt_root_hex);
         console.log("receipt_hex: " + receipt_hex);
         console.log("receiptmerkleproof_hex: " + receiptmerkleproof_hex);
         console.log("block number: " + blockNumber);
         try {
-          let results = await axios.get('https://' + CONFIGURATION.agentURL + ':' + CONFIGURATION.agentPort + '/syscoinrpc?method=assetallocationmint&asset=' + toSysAssetGUID + '&address=' + syscoinWitnessAddress + '&amount=' + toSysAmount + '&blocknumber=' + blockNumber + '&bridgetransferid=' + bridgeTransferId + '&tx_hex=' + tx_hex + '&txroot_hex=' + tx_root_hex + '&txmerkleproof_hex=' + txmerkleproof_hex + '&txmerkleproofpath_hex=' + txmerkleproofpath_hex + '&receipt_hex=' + receipt_hex + '&receiptroot_hex=' + receipt_root_hex + '&receiptmerkleproof_hex=' + receiptmerkleproof_hex);
+          let results = await axios.get('https://' + CONFIGURATION.agentURL + ':' + CONFIGURATION.agentPort + '/syscoinrpc?method=assetallocationmint&asset=' + toSysAssetGUID + '&address=' + syscoinWitnessAddress + '&amount=' + toSysAmount + '&blocknumber=' + blockNumber + '&bridgetransferid=' + bridgeTransferId + '&tx_hex=' + tx_hex + '&txmerkleproof_hex=' + txmerkleproof_hex + '&txmerkleproofpath_hex=' + txmerkleproofpath_hex + '&receipt_hex=' + receipt_hex + '&receiptroot_hex=' + receiptmerkleproof_hex);
           results = results.data;
           if(results.error){
             validateNewInput.buttonVal = false;
