507ca543a58524daa4f893c3bd991eebdd4273ee
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-07-14 12:34:28 -0700

    update contracts

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@
       this.sysRPCUser = "u";
       this.sysRPCPassword = "p";
       this.web3URL = "http://localhost:8101";
-      this.relayContract = "0xFd282b0D627130E25963927Badc139bDC4Fd1A44";
-      this.ERC20Manager = "0x000BB0813038bd7766489c5E6A098263fA4D9Ede";
+      this.relayContract = "0xD822557aC2F2b77A1988617308e4A29A89Cb95A6";
+      this.ERC20Manager = "0xA738a563F9ecb55e0b2245D1e9E380f0fE455ea1";
       this.tokenFreezeFunction = "0x7ca654cf9212e4c3cf0164a529dd6159fc71113f867d0b09fdeb10aa65780732";
       this.NEVMAddressExplorerURL = "https://etherscan.io/address/";
       this.SyscoinTxExplorerURL = "https://sys1.bcfn.ca/tx/";
